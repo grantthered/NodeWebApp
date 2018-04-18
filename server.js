d9050b6a0e2e8cd6fba01eb259b32664247fa8d0
grantthered@gmail.com <grantthered@gmail.com>
2018-04-18 09:39:09 -0500

    Added projects page.

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+//Load in modules
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+//Get port from environment variable if possible. Necessary to work with Heroku.
 const port = process.env.PORT || 3000;
 
+//Express is a web framework for node
 var app = express();
 
+//Tell Handlebars where to find partial views.
 hbs.registerPartials(__dirname + '/views/partials');
+
+//Let Express know that we're using Handlebars.
 app.set('view engine', 'hbs');
 
+//app.use(middleware_layer_here)
+//in this case, log every request
 app.use((req, res, next) => {
   var now = new Date().toString();
   
@@ -22,12 +30,10 @@ app.use((req, res, next) => {
   next();
 });
 
-//app.use((req, res, next) => {
-//  res.render('maintenance.hbs');
-//});
-
+//registers things in the public folder for viewing
 app.use(express.static(__dirname + '/public'));
 
+//registers helper function that handlebars can use
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
@@ -36,6 +42,8 @@ hbs.registerHelper('screamIt', (text) => {
   return text.toUpperCase();
 });
 
+//app.get is used to add GET method routes
+//app.post for POST method routes
 app.get('/', (req, res) => {
   res.render('home.hbs', {
       pageTitle: 'Home Page',
@@ -55,6 +63,13 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
+//listens at given port
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
